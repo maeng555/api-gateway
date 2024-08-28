87e6bc84ae223f172bbef76cbccceaa41c722463
maeng555 <mjy5188@naver.com>
2024-08-29 01:34:54 +0900

    api gateway 생성

---
+++ src/main/kotlin/gyus/api_gateway/ApiGatewayApplication.kt
@@ -0,0 +1,11 @@
+package gyus.api_gateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiGatewayApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiGatewayApplication>(*args)
+}
